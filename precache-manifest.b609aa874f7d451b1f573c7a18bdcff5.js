1dbdacd1bed6b8de82f76580a68b442418e5c201
sumit <sharmasumit0308@gmail.com>
2020-07-15 12:45:14 -0400

    Updates

---
+++ precache-manifest.b609aa874f7d451b1f573c7a18bdcff5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cfc96ef559582f9cc9539733c0e6826",
+    "url": "/.github.io/Login_Assignment/index.html"
+  },
+  {
+    "revision": "66acf852ffbcf6ef7fcc",
+    "url": "/.github.io/Login_Assignment/static/css/2.30c2d978.chunk.css"
+  },
+  {
+    "revision": "257a6ebcdda7b540601c",
+    "url": "/.github.io/Login_Assignment/static/css/main.c2b73c47.chunk.css"
+  },
+  {
+    "revision": "66acf852ffbcf6ef7fcc",
+    "url": "/.github.io/Login_Assignment/static/js/2.07511fcb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/.github.io/Login_Assignment/static/js/2.07511fcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "257a6ebcdda7b540601c",
+    "url": "/.github.io/Login_Assignment/static/js/main.b4504245.chunk.js"
+  },
+  {
+    "revision": "cc1986fe87ed7748b54a",
+    "url": "/.github.io/Login_Assignment/static/js/runtime-main.5f7af217.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/.github.io/Login_Assignment/static/media/logo.5d5d9eef.svg"
+  }
+]);
